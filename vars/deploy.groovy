230793c799f04fe0c312d67762a3b52a5909ab09
samuelTani21022000 <samueltanifrancisco@gmail.com>
2025-04-02 20:32:18 -0300

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,1 +1,10 @@
-
+def call() {
+    stage('Deploy with PM2') {
+        echo '🚀 Iniciando deploy com PM2'
+        if (isUnix()) {
+            sh 'pm2 startOrRestart pm2.config.json'
+        } else {
+            bat 'pm2 startOrRestart pm2.config.json'
+        }
+    }
+}
