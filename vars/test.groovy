686225e62ef7bf4ce69a499b3cdd8b7d4a55d443
samuelTani21022000 <samueltanifrancisco@gmail.com>
2025-04-02 20:32:49 -0300

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,1 +1,10 @@
-
+def call() {
+    stage('Install PM2') {
+        echo '⚙️ Instalando PM2 globalmente'
+        if (isUnix()) {
+            sh 'npm install pm2 -g'
+        } else {
+            bat 'npm install pm2 -g'
+        }
+    }
+}
