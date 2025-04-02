be1e53c8569ee94e0dc5f319347d8ef2783f2d3c
samuelTani21022000 <samueltanifrancisco@gmail.com>
2025-04-02 20:31:50 -0300

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,1 +1,19 @@
-
+def call() {
+    stage('Build Setup') {
+        echo '🔧 Configurando ambiente de build'
+        checkout([
+            $class: 'GitSCM',
+            branches: [[name: 'main']],
+            userRemoteConfigs: [[url: 'https://github.com/MIRTAHAALI/express_server_for_flutter_app_testing.git']]
+        ])
+    }
+    
+    stage('Install Dependencies') {
+        echo '📦 Instalando dependências'
+        if (isUnix()) {
+            sh 'npm install'
+        } else {
+            bat 'npm install'
+        }
+    }
+}
